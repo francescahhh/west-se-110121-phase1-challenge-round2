bd200828640e438376368581eda50bb7ac845b44
Francesca Hamm <francesca.c.hamm@gmail.com>
2021-11-15 10:35:31 -0700

    submission for deadline

--- src/index.js
+++ src/index.js
@@ -1,1 +1,59 @@
-console.log("you got this!");
+const flortagormAPI = "https://distinct-vaulted-freesia.glitch.me/image"
+let imageData;
+
+//comment list variable
+const commentList = document.getElementById('fg-comments');
+
+//fetch request
+fetch(flortagormAPI)
+    .then((response) => response.json())
+    .then((json) => {imageData = json;
+    renderFlortagorm(imageData)
+});
+
+//like event listener
+document.getElementById('like-button').addEventListener ('click', incrementLikes);
+
+//comment form event listener
+document.getElementById('comment-form').addEventListener('submit', addComment);
+
+//full image card function 
+function renderFlortagorm(image) {
+    const title = document.getElementById('fg-title');
+    title.textContent = image.title;
+
+    document.getElementById('fg-image').src = image.image;
+
+    renderLikes(image.likes);
+
+    renderComments(image.comments);
+}
+
+//like functions
+function incrementLikes() {
+    imageData.likes += 1;
+    renderLikes(imageData.likes);
+}
+
+function renderLikes(likes) {
+    document.getElementById('fg-likes').textContent = `${likes} likes`;
+}
+
+//comment functions
+function addComment(event) {
+    event.preventDefault();
+    const commentText = event.target.comment.value;
+    renderComment({content: commentText});
+    event.target.reset();
+} 
+
+function renderComments(comments) {
+    commentList.innerHTML = '';
+    comments.forEach(renderComment);
+}
+
+function renderComment(comment) {
+    const listItem = document.createElement('li');
+    listItem.textContent = comment.content;
+    commentList.append(listItem);
+}
